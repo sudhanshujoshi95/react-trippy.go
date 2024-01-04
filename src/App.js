78eb4caf2dc9eb0af8c58dd7a2dcef2224d35c9a
Sudhanshu <sudhanshujoshi95@gmail.com>
2024-01-04 23:57:11 +0530

    browser-router issue fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
 
-      <HashRouter>
+      <HashRouter basename='/react-trippy.go'>
         <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
