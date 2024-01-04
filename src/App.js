d529ba2c9268be0695ada7a8027078e2e782e8ea
Sudhanshu <sudhanshujoshi95@gmail.com>
2024-01-04 23:59:57 +0530

    browser-router issue fixed1

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Contact from './routes/Contact';
 import Home from "./routes/Home";
 import Service from './routes/Service';
 import About from './routes/About';
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
 
-      <HashRouter basename='/react-trippy.go'>
+      <BrowserRouter basename='/react-trippy.go'>
         <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/service" element={<Service/>}/>
         <Route path="/contact" element={<Contact/>}/>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
 
     </div>
   );
