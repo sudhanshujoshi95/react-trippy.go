8288db33b51f9461642a355d23debc0f05a25199
Sudhanshu <sudhanshujoshi95@gmail.com>
2024-01-04 23:40:31 +0530

    browser-router issue fixed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Contact from './routes/Contact';
 import Home from "./routes/Home";
 import Service from './routes/Service';
 import About from './routes/About';
-import {Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
 
-      <Routes>
+      <HashRouter>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/service" element={<Service/>}/>
         <Route path="/contact" element={<Contact/>}/>
-      </Routes>
+      </HashRouter>
 
     </div>
   );
